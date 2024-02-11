5ad781a3fd5a007019c62a9e8c9cf7236ce77e75
D0M1 <giulio.dominutti@gmail.com>
2024-02-11 20:41:43 +0100

    cleanup

--- projects/ngx-touch-virtual-keyboard-demo/src/app/app.module.ts
+++ projects/ngx-touch-virtual-keyboard-demo/src/app/app.module.ts
@@ -3,12 +3,12 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {NgxTouchVirtualKeyboardModule} from 'ngx-touch-virtual-keyboard';
+import {ICON_KEYBOARD, NgxTouchVirtualKeyboardModule} from 'ngx-touch-virtual-keyboard';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, NgxTouchVirtualKeyboardModule],
-  providers: [],
+  providers: [{provide: ICON_KEYBOARD, useValue: 'assets/icons/keyboard-test.svg'}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
